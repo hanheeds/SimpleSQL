e5551fe3c4a2ad0c2997a9e2fde8968a5b488ae3
hanheeds <123617674+hanheeds@users.noreply.github.com>
2023-03-17 17:08:53 -0500

    Add files via upload

---
+++ execute.c
@@ -0,0 +1,26 @@
+/*execute.c*/
+
+//
+// Project: Execution of queries for SimpleSQL
+//
+// YOUR NAME
+// Northwestern University
+// CS 211, Winter 2023
+//
+
+#include <stdbool.h> // true, false
+#include <stdio.h>
+#include <stdlib.h>
+//
+// #include any other system <.h> files?
+//
+
+#include "execute.h"
+//
+// #include any other of our ".h" files?
+//
+
+
+//
+// implementation of function(s), both private and public
+//
---
+++ execute.h
@@ -0,0 +1,19 @@
+/*execute.h*/
+
+//
+// Project: Execution of queries for SimpleSQL
+//
+// YOUR NAME
+// Northwestern University
+// CS 211, Winter 2023
+//
+
+#pragma once
+
+//
+// #include header files needed to compile function declarations
+//
+
+//
+// function declarations:
+//
